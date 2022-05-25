9ceddc933df5004a3f7b2875ef5ff417804ddcab
phamle21 <phamle21@gmail.com>
2022-05-25 12:48:22 +0700

    ddone

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -40,6 +40,8 @@
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.0/css/jquery.dataTables.css">
 
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.js"></script>
